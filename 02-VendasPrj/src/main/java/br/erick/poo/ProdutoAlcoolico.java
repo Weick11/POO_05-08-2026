ab0525b22e7af2e3ba6f798917337bbc52325689
Weick11 <ericksouzamoraes77@gmail.com>
2026-08-26 14:19:42 +0000

    Herança Produto

--- 02-VendasPrj/src/main/java/br/erick/poo/ProdutoAlcoolico.java
+++ 02-VendasPrj/src/main/java/br/erick/poo/ProdutoAlcoolico.java
@@ -1,18 +1,19 @@
 package br.erick.poo;
 
 public class ProdutoAlcoolico extends Produto{
-    private final double TAXA_ALCOOLICO = 2;
+    private final double TAXA_TRAGO = 2;
 
     public ProdutoAlcoolico(int codigo, String nome, double preco){
         super(codigo, nome, preco);
     }
 
     public double getTAXA(){
-        return TAXA_ALCOOLICO;
+        return TAXA_TRAGO;
     }
 
+    @Override
     public double getPreco(){
-        return super.getPreco() * TAXA_ALCOOLICO;
+        return super.getPreco() * TAXA_TRAGO;
     }
 
     public String toString(){
